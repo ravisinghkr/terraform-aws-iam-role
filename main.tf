d7a63dbdbea3bb6ecaecdb962f3b8b42d9b66b27
Ravi Singh <ravi.singh@reedbusiness.com>
2021-08-14 02:18:45 +0100

    Adding files to create terraform module

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "role" {
+  name               = var.name
+  description        = var.description
+  assume_role_policy = var.assumed_role_policy
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = var.name
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachment" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_policy" "policy" {
+  name = "${var.name}_policy_attach"
+  policy = var.role_policy
+}
+
+resource "aws_iam_role_policy_attachment" "common_policy_attachment" {
+for_each = toset(var.existing_policy_names_to_attach)
+  role       = aws_iam_role.role.name
+  policy_arn = "aws_iam_policy.${each.key}.arn"
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable name {
+    description = "Name of the role to be created"
+}
+
+variable description {
+    description = "Description about the role to be created"
+    default = ""
+}
+
+variable assumed_role_policy {
+    description = "Policy for role to assume"
+}
+
+variable role_policy {
+    description = "Policy to be associated to the role"
+}
+
+variable existing_policy_names_to_attach {
+    description = "Any existing policies to be associated to the role. Should be privided in form of a list"
+    default=[]
+}
